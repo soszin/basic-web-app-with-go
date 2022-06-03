d6b7927700c0b20694aea8597f2231bcf7a5a9ec
soszin <d.sroczynski92@gmail.com>
2022-06-03 14:02:31 +0200

    Serving HTML Templates

--- main.go
+++ main.go
@@ -1,63 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-// Home is the home page handler
+// Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
-// About is the about page handler
+// About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := AddValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-// AddValues adds two integers and return the sum
-func AddValues(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 0.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
+		fmt.Println("error parsing template:", err)
 	}
-
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f devided by %f is %f", 100.0, 0.0, f))
 }
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-// main is the main application function
+// main is the main function
 func main() {
-
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
 }
-
-//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-//	n, err := fmt.Fprintf(w, "Hello, world!")
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-//})
