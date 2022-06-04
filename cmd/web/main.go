3cc55edc87a1a92f641c4aba6f9a50a39ee618aa
soszin <d.sroczynski92@gmail.com>
2022-06-04 21:26:09 +0200

    State Management with Sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/soszin/go-course/pkg/config"
 	"github.com/soszin/go-course/pkg/handlers"
 	"github.com/soszin/go-course/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
